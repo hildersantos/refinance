91b00000bee9616c5020ad41caa546d357e81563
Hilder Santos <hilder@hildersantos.com>
2018-09-04 18:15:22 -0300

    Mocking Transactions Table

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,37 @@
 import React, { Component } from "react";
-import { Menu } from "semantic-ui-react";
+import { Menu, Grid, Container, Table } from "semantic-ui-react";
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
         <Menu fixed="top" color="black" inverted>
-          <Menu.Item header name="refinance" />
+          <Container>
+            <Menu.Item header name="refinance" />
+          </Container>
         </Menu>
+        <Grid container>
+          <Grid.Row>
+            <Grid.Column>
+              <Table striped>
+                <Table.Header>
+                  <Table.Row>
+                    <Table.HeaderCell>Type</Table.HeaderCell>
+                    <Table.HeaderCell>Description</Table.HeaderCell>
+                    <Table.HeaderCell>Value</Table.HeaderCell>
+                  </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                  <Table.Row>
+                    <Table.Cell>R</Table.Cell>
+                    <Table.Cell>A Description</Table.Cell>
+                    <Table.Cell>R$ 15,00</Table.Cell>
+                  </Table.Row>
+                </Table.Body>
+              </Table>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
       </React.Fragment>
     );
   }
